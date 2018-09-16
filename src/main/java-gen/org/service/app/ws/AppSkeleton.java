62959cced4de8c1f27156474ef3ef33abb614504
Anish <Anish@Anish>
2018-09-16 09:36:11 -0700

    modified

---
+++ src/main/java-gen/org/service/app/ws/AppSkeleton.java
@@ -0,0 +1,26 @@
+/**
+ * AppSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.3  Built on : May 30, 2016 (04:08:57 BST)
+ */
+package org.service.app.ws;
+
+
+/**
+ *  AppSkeleton java skeleton for the axisService
+ */
+public class AppSkeleton {
+    /**
+     * Auto generated method signature
+     *
+     * @param newOperation
+     * @return newOperationResponse
+     */
+    public org.service.www.app.NewOperationResponse newOperation(
+        org.service.www.app.NewOperation newOperation) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#newOperation");
+    }
+}
